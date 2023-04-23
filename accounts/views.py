74e8c02ed12212b9005fcd2b7b0784e51f93f29a
toshi <d-toshi@i.softbank.jp>
2023-04-23 11:02:43 +0900

    event詳細、グループカレンダー実装、他修正

--- accounts/views.py
+++ accounts/views.py
@@ -30,42 +30,46 @@ class ProfileView(OnlyYouMixin, DetailView):
         user_data = CustomUser.objects.get(email=self.request.user)
         group_data_m = ApprovedMember.objects.filter(member=user_data, approved=True)
         group_data_s = ApprovedStaff.objects.filter(staff=user_data, approved=True)
+        event_join=Join.objects.filter(join_name=self.request.user, join=True).order_by('join_event__event_date')
         # print(user_data.applyingmember_set.all())
         # applyings_m = user_data.applyingmember_set.all()
         # for applying_m in applyings_m:
         #     print(applying_m.group)
-        join_event = Join.objects.filter(join_name=self.request.user, join=True).order_by('join_event__event_date')
         return render(request, self.template_name,{
             'group_data_m':group_data_m,
             'group_data_s':group_data_s,
             'user_data':user_data,
-            'join_event':join_event,
+            'event_join':event_join,
         })
 
     def post(self, request, *args, **kwargs):
-        user_data = CustomUser.objects.get(email=self.request.user)
-        pk=user_data.pk
-        if "applying_m_group" in request.POST:
+        if 'applying_m_group' in request.POST: #メンバー申請取消
+            user_data = CustomUser.objects.get(email=self.request.user)
+            pk=user_data.pk
             applying_member_pks = request.POST.getlist('applying_m_group')
             print(applying_member_pks)
             applying_member = ApplyingMember.objects.filter(pk__in=applying_member_pks, applying=True)
             print(applying_member)
             applying_member.delete()
-
-        if "applying_s_group" in request.POST:
+            return HttpResponseRedirect( reverse_lazy('userprofile', kwargs={'pk':pk}))
+        elif 'applying_s_group' in request.POST: #スタッフ申請取消
+            user_data = CustomUser.objects.get(email=self.request.user)
+            pk=user_data.pk
             applying_staff_pks = request.POST.getlist('applying_s_group')
             print(applying_staff_pks)
             applying_staff = ApplyingStaff.objects.filter(pk__in=applying_staff_pks, applying=True)
             print(applying_staff)
             applying_staff.delete()
-            
-        elif "event_join" in request.POST:
-            join_pks = request.POST.getlist('event_join')
-            join = Join.objects.filter(pk__in=join_pks, join=True)
-            join.delete()
-            
-        return HttpResponseRedirect( reverse_lazy('userprofile', kwargs={'pk':pk}))
-        
+            return HttpResponseRedirect( reverse_lazy('userprofile', kwargs={'pk':pk}))
+        elif 'event_join' in request.POST: #イベント予約取消
+            user_data = CustomUser.objects.get(email=self.request.user)
+            pk=user_data.pk
+            event_join_pks = request.POST.getlist('event_join')
+            print(event_join_pks)
+            event_join = Join.objects.filter(pk__in=event_join_pks, join=True)
+
+            event_join.delete()
+            return HttpResponseRedirect( reverse_lazy('userprofile', kwargs={'pk':pk}))
 
 class ProfileEditView(OnlyYouMixin, UpdateView):
     model = CustomUser
