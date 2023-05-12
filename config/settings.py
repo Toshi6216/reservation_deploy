874bcf48b3a58f48218f8459b1bef2069dd995e4
toshi <d-toshi@i.softbank.jp>
2023-05-12 10:17:56 +0900

    イベント削除ボタン追加、settings修正等

--- config/settings.py
+++ config/settings.py
@@ -29,19 +29,33 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 
-from django.core.management.utils import get_random_secret_key
-SECRET_KEY = get_random_secret_key()
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
-DEBUG = True
+try:
+    from .local_settings import *
+    DEBUG = True
+    FRONTEND_URL = 'http://127.0.0.1:8000/'
+    ALLOWED_HOSTS = []
+except ImportError:
+    DEBUG = False
+    from django.core.management.utils import get_random_secret_key
+    SECRET_KEY = get_random_secret_key()
+    FRONTEND_URL = 'https://htoshijapan.pythonanywhere.com/'
+    ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['.pythonanywhere.com']
+# メールサーバー用
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'app.info.service.ht@gmail.com'
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 
+
+
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -188,20 +202,14 @@ ACCOUNT_FORMS = {
 }
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #メールのコンソール表示　デバッグ用
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' #メールのコンソール表示　本番用
-
-
-
-
-FRONTEND_URL = 'http://127.0.0.1:8000/'
-FRONTEND_URL = 'http://htoshijapan.pythonanywhere.com/'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' #メールのコンソール表示　本番用
 
 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/assets/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
